c069beccc3ae45cebc32791a43954b37e8055adf
Hoang <nickhoang96@hotmail.com>
2018-01-29 18:58:10 -0600

    Revert "added mult"
    This reverts commit 0e6e8ed4c3f266df8bf08d842f0657a62c49d90f.

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 
-int multiply(int x, int y);
+int add(int x, int y);
 
 int main()
 {
+	int a,b,c;
+	std::cout<< "Enter two number here: "<< std::endl;
+	std::cin>> a >> b;
+	c = add(a,b);
+	std::cout << "The resulting addition of the function is "<< c << std::endl;
 	std::cout<< "Hello, World << std::endl;
 	return 0;
 }
 
-int multiply(int x, int y)
+int add(int x, int y)
 {
-	return x * y;
+	return x + y;
 }
