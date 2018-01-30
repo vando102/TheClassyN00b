0e6e8ed4c3f266df8bf08d842f0657a62c49d90f
Hoang <nickhoang96@hotmail.com>
2018-01-29 18:57:27 -0600

    added mult

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,19 +1,14 @@
 #include <iostream>
 
-int add(int x, int y);
+int multiply(int x, int y);
 
 int main()
 {
-	int a,b,c;
-	std::cout<< "Enter two number here: "<< std::endl;
-	std::cin>> a >> b;
-	c = add(a,b);
-	std::cout << "The resulting addition of the function is "<< c << std::endl;
 	std::cout<< "Hello, World << std::endl;
 	return 0;
 }
 
-int add(int x, int y)
+int multiply(int x, int y)
 {
-	return x + y;
+	return x * y;
 }
